772e5efaef64bc26b65543d238786e516bf6a1bf
jessieshao <505133853@qq.com>
2018-09-09 22:47:53 +0800

    first commit

---
+++ app/index.js
@@ -0,0 +1,9 @@
+// index.js
+//var sum = require('./sum')
+//console.log(sum(1, 2))
+
+import sum from './sum'
+import smallImg from './addiamges'
+import bigImg from './addiamges'
+import '../css/a.scss'
+console.log(sum(1, 2))
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+// 自带的库
+const path = require('path')
+module.exports = {
+    entry:  './app/index.js', // 入口文件
+    output: {
+      path: path.resolve(__dirname, 'build'), // 必须使用绝对地址，输出文件夹
+      filename: "bundle.js" ,// 打包后输出文件的文件名
+      publicPath: 'build/' // 知道如何寻找资源
+
+    },
+    module: {
+      rules: [
+          {
+          // js 文件才使用 babel
+              test: /\.js$/,
+           // 使用哪个 loader
+              use: 'babel-loader',
+          // 不包括路径
+              exclude: /node_modules/
+          },
+          {
+            test: /\.scss$/,
+            use: [
+                "style-loader", // creates style nodes from JS strings
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            ]
+          },
+          {
+            // 图片格式正则
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                use: [
+                  {
+                    loader: 'url-loader',
+                    // 配置 url-loader 的可选项
+                    options: {
+                    // 限制 图片大小 10000B，小于限制会将图片转换为 base64格式
+                      limit: 1,
+                    // 超出限制，创建的文件格式
+                    // build/images/[图片名].[hash].[图片格式]
+                      name: 'images/[name].[hash].[ext]'
+                   }
+                  }
+                ]
+            }
+      ]
+    }
+  }
+  
